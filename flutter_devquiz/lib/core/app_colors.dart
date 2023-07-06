a95337bb406c3433530d0cfc5f36475e26c39f43
JDaniloC <jdsc@cin.ufpe.br>
2023-07-05 23:25:37 -0300

    Add core: colors, gradients, images and styles

---
+++ flutter_devquiz/lib/core/app_colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color purple = Color(0xFF8257E5);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF514766);
+  static const Color gray = Color(0xFF6E6680);
+  static const Color lightGrey = Color(0xFFA6A1B2);
+  static const Color border = Color(0xFFE1E1E6);
+  static const Color chartSecondary = Color(0xFFE1E6E3);
+  static const Color chartPrimary = darkGreen;
+
+  //Greens
+  static const Color lightGreen = Color(0xFFE1F5EC);
+  static const Color green = Color(0xFFB8DBCB);
+  static const Color darkGreen = Color(0xFF04D361);
+
+  //Reds
+  static const Color lightRed = Color(0xFFF5E9EC);
+  static const Color red = Color(0xFFE5C5CF);
+  static const Color darkRed = Color(0xFFCC3750);
+
+  //LevelButton
+  static const Color levelButtonEasy = Color(0xFFEBEBFC);
+  static const Color levelButtonRegular = lightGreen;
+  static const Color levelButtonHard = Color(0xFFF5EFE9);
+  static const Color levelButtonHardest = lightRed;
+
+  static const Color levelButtonBorderEasy = Color(0xFFCECEF5);
+  static const Color levelButtonBorderRegular = green;
+  static const Color levelButtonBorderHard = Color(0xFFE5D5C5);
+  static const Color levelButtonBorderHardest = red;
+
+  static const Color levelButtonTextEasy = Color(0xFF6363DB);
+  static const Color levelButtonTextRegular = darkGreen;
+  static const Color levelButtonTextHard = Color(0xFFE8891C);
+  static const Color levelButtonTextHardest = darkRed;
+}
